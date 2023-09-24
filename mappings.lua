834ff9b0f18a980636dade1f54861642055cefd0
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2023-09-24 16:07:22 +0300

    Adds the custom keybidings

--- mappings.lua
+++ mappings.lua
@@ -4,6 +4,14 @@ local M = {}
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<F5>"] = {
+      "<cmd> make <CR>",
+      "Make Compile program"
+    }
+  },
+  i = {
+    ["<F5>"]  = { "<cmd> w <CR> <cmd> make <CR>", "Save and Make Compile program" },
+    ["<C-s>"] = { "<cmd> w <CR>", "Save file" },
   },
 }
 
